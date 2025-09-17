213eb36d6b14fd8590e988845e663aacf817ed79
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-09-18 01:52:52 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl fo'>
       
     </div>
   )
